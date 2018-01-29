cf087160c586e211a391ab17e54647b368d95d3f
Anurag Regmi <anurag.regmi@aayulogic.com>
2018-01-29 18:55:35 +0545

    Use mixin

--- music/serializers.py
+++ music/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from music.mixins import WritableNestedModelSerializer
 from music.models import Track, Album
 
 
@@ -10,41 +11,10 @@ class TrackSerializer(serializers.ModelSerializer):
         read_only_fields = ('album',)
 
 
-class AlbumSerializer(serializers.ModelSerializer):
+class AlbumSerializer(WritableNestedModelSerializer):
     tracks = TrackSerializer(many=True)
 
     class Meta:
         model = Album
         fields = ('id', 'album_name', 'artist', 'tracks')
-
-    def create(self, validated_data):
-        tracks_data = validated_data.pop('tracks')
-        album = Album.objects.create(**validated_data)
-        for track_data in tracks_data:
-            Track.objects.create(album=album, **track_data)
-        return album
-
-    def update(self, instance, validated_data):
-        validated_data.pop('tracks')
-        tracks_data = self.initial_data.get('tracks')
-        if tracks_data:
-            for track_data in tracks_data:
-                tr_id = track_data.get('id')
-                if tr_id:
-                    try:
-                        track = Track.objects.get(id=tr_id)
-                        track_sr = TrackSerializer(instance=track, data=track_data, partial=self.partial)
-                        if track_sr.is_valid():
-                            track_sr.save()
-                        else:
-                            raise serializers.ValidationError(track_sr.data)
-                    except Track.DoesNotExist:
-                        raise serializers.ValidationError('No object found to update for id={}'.format(tr_id))
-                    except serializers.ValidationError as e:
-                        raise e
-                else:
-                    Track.objects.create(album=instance, **track_data)
-
-        instance = super().update(instance, validated_data)
-
-        return instance
+        writable_nested_fields = {'tracks': 'album'}
